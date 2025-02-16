c4659f4b3286793c1581b256330e8800aa48e238
zhengke <zhengke996@163.com>
2025-02-17 00:13:30 +0800

    ci: 配置 GitHub Pages 自定义域名并调整项目基础路径
    - 在 build-deploy.yml 中添加自定义域名配置项
    - 新增 CNAME 文件，指定自定义域名为 jinhua-tourism.timu.fun
    - 修改 vite.config.ts，将项目基础路径从 "/vue-ts-tailwind4-self-adaptation/" 改为 "/"

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: "/vue-ts-tailwind4-self-adaptation/",
+  base: "/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
